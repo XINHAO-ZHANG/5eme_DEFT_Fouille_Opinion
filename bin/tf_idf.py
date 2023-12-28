da4f2ca0ed6970a25ecda9ab5947b4b36b143fc0
XINHAO-ZHANG <zhangxinhao672@gmail.com>
2023-12-28 02:22:12 +0100

    petite_modif

--- bin/tf_idf.py
+++ bin/tf_idf.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
 
 def tfidfVectorize(CSV_train, CSV_test):
     '''
@@ -10,17 +11,28 @@ def tfidfVectorize(CSV_train, CSV_test):
     train_df = pd.read_csv(CSV_train)
     test_df = pd.read_csv(CSV_test)
 
+    # remplacer les valeurs manquantes par des chaînes vides
+    train_df['texte'].fillna('', inplace=True)
+    test_df['texte'].fillna('', inplace=True)
+
+    # initialiser le vectorizer avec les paramètres désirés
     vectorizer = TfidfVectorizer(
         ngram_range=(1, 3),
         max_df=0.5,
         use_idf=True,
         sublinear_tf=True
+        max_features=10000
     )
-    X_train = vectorizer.fit_transform(train_df['texte'])
-    X_test = vectorizer.transform(test_df['texte'])
+
+    # vectoriser les textes d'entraînement et de test 
+    print("Vectorizing train data...")
+    X_train = vectorizer.fit_transform(tqdm(train_df['texte']))
+
+    print("Vectorizing test data...")
+    X_test = vectorizer.transform(tqdm(test_df['texte']))
 
     # récupérer les labels correspondants
-    y_train = train_df['parti']
-    y_test = test_df['parti']
+    y_train = train_df['parti'].astype(str)
+    y_test = test_df['parti'].astype(str)
 
     return X_train, X_test, y_train, y_test
